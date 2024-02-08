6b9cb0a674d44a73dbae2fc5d1b14e05e79c12c9
JacobHA <jacobhadamczyk@gmail.com>
2024-02-07 19:40:47 -0500

    gather on actions first

--- bound_utils.py
+++ bound_utils.py
@@ -1,21 +1,28 @@
 import torch as th
 
 
-def net_to_delta(gamma, rewards, dones, next_q_values, curr_q_values):
+def net_to_delta(gamma, rewards, dones, actions, next_q_values, curr_q_values):
     next_v_max, _ = next_q_values.max(dim=1, keepdim=True)
     delta = rewards + \
         (1 - dones) * \
         gamma * next_v_max - curr_q_values
-    delta = delta.reshape(-1, 1)
+    # delta = delta.reshape(-1, 1)
+    delta = delta.gather(1, actions)
+
     delta_min = th.min(delta)
+    # gather delta at actions:
+    # delta_min = delta.min(dim=1, keepdim=True)[0]
+    # delta_min = th.topk(delta, 3, largest=False).values[-1]
     delta_max = th.max(delta)
+    # delta_max = th.topk(delta, 3, largest=True).values[-1]
+    # delta_max = delta.max(dim=1,keepdim=True)[0]
     return delta, delta_min, delta_max
 
-def bounds(beta, gamma, rewards, dones, next_q_values, curr_q_values):
+def bounds(beta, gamma, rewards, dones, actions, next_q_values, curr_q_values):
     lb = -th.ones_like(rewards) * float('inf')
     ub = th.ones_like(rewards) * float('inf')
     for (next_q_value, curr_q_value) in zip(next_q_values, curr_q_values):
-        delta, delta_min, delta_max = net_to_delta(gamma, rewards, dones, next_q_value, curr_q_value)
+        delta, delta_min, delta_max = net_to_delta(gamma, rewards, dones, actions, next_q_value, curr_q_value)
         # V = next_q_value.max(dim=1)[0].reshape(-1, 1)
         nA = next_q_value.shape[-1]
         device = next_q_value.device
