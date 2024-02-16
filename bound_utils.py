b5aca7d60a243e0d8cd46f16b2de9f453445260e
JacobHA <jacobhadamczyk@gmail.com>
2024-02-16 11:13:34 -0500

    new bounds with absorbing states taken into account

--- bound_utils.py
+++ bound_utils.py
@@ -1,36 +1,39 @@
 import torch as th
 
 
-def net_to_delta(gamma, rewards, dones, actions, next_q_values, curr_q_values):
-    next_v_max, _ = next_q_values.max(dim=1, keepdim=True)
-    delta = rewards + \
-        (1 - dones) * \
-        gamma * next_v_max - curr_q_values
+def net_to_delta(beta, gamma, rewards, dones, actions, next_q_values, curr_q_values):
+    # next_v_max, _ = next_q_values.max(dim=1, keepdim=True)
+    nA = next_q_values.shape[-1]
+    device = next_q_values.device
+    V = 1/beta * th.logsumexp(next_q_values * beta, dim=1, keepdim=True) - th.log(th.tensor([nA], device=device))
+
+    delta = rewards + gamma * V - curr_q_values
     # delta = delta.reshape(-1, 1)
     delta = delta.gather(1, actions)
 
-    delta_min = th.min(delta)
+    delta_min = th.min(delta[dones == 0])
     # gather delta at actions:
     # delta_min = delta.min(dim=1, keepdim=True)[0]
     # delta_min = th.topk(delta, 3, largest=False).values[-1]
-    delta_max = th.max(delta)
+    delta_max = th.max(delta[dones == 0])
     # delta_max = th.topk(delta, 3, largest=True).values[-1]
     # delta_max = delta.max(dim=1,keepdim=True)[0]
     return delta, delta_min, delta_max
 
 def bounds(beta, gamma, rewards, dones, actions, next_q_values, curr_q_values):
-    lb = -th.ones_like(rewards) * float('inf')
-    ub = th.ones_like(rewards) * float('inf')
-    for (next_q_value, curr_q_value) in zip(next_q_values, curr_q_values):
-        delta, delta_min, delta_max = net_to_delta(gamma, rewards, dones, actions, next_q_value, curr_q_value)
-        # V = next_q_value.max(dim=1)[0].reshape(-1, 1)
-        nA = next_q_value.shape[-1]
-        device = next_q_value.device
-        V = 1/beta * th.logsumexp(next_q_value * beta, dim=1, keepdim=True) - th.log(th.tensor([nA], device=device))
-        lb = rewards + gamma * (V + delta_min / (1 - gamma) )
-        ub = rewards + gamma * (V + delta_max / (1 - gamma) )
-        # Successively take the better bound:
-        lb = th.max(lb, lb)
-        ub = th.min(ub, ub)
-    
-    return lb, ub
+    with th.no_grad():
+        lb = -th.ones_like(rewards) * float('inf')
+        ub = th.ones_like(rewards) * float('inf')
+        for (next_q_value, curr_q_value) in zip(next_q_values, curr_q_values):
+            delta, delta_min, delta_max = net_to_delta(beta, gamma, rewards, dones, actions, next_q_value, curr_q_value)
+            # V = next_q_value.max(dim=1)[0].reshape(-1, 1)
+            nA = next_q_value.shape[-1]
+            device = next_q_value.device
+            V = 1/beta * th.logsumexp(next_q_value * beta, dim=1, keepdim=True) - th.log(th.tensor([nA], device=device))
+            lb = rewards + gamma * (V + delta_min / (1 - gamma) ) * (1 - dones)
+            ub = rewards + gamma * (V + delta_max / (1 - gamma) ) * (1 - dones)
+            # Successively take the better bound:
+            lb = th.max(lb, lb)
+            ub = th.min(ub, ub)
+        
+        return lb, ub
--- experiment.py
+++ experiment.py
@@ -1,21 +1,55 @@
 from BoundedSQL import SoftQAgent
 import wandb
 
+from tabular import ModifiedFrozenLake
+from gymnasium.wrappers import TimeLimit
+
 sql_cpole = {
     'batch_size': 64,
-    'beta': 0.1,
+    'beta': 1,
     'gamma': 0.98,
     'hidden_dim': 64,
     'learning_rate': 0.01,
-    'learning_starts': 0.02*50_000,
+    'learning_starts': 1000,#0.02*50_000,
     'target_update_interval': 100,
     'tau': 0.95,
     'train_freq': 9,
     'gradient_steps': 9,
 }
 
+sql_lunar = {
+    'batch_size': 128,
+    'beta': 10,
+    'gamma': 0.98,
+    'hidden_dim': 128,
+    'learning_rate': 0.003,
+    'learning_starts': 0.023*500_000,
+    'target_update_interval': 1000,
+    'tau': 0.92,
+    'train_freq': 5,
+    'gradient_steps': 5,
+}
+
+sql_mcar = {
+    'batch_size': 128,
+    'beta': 0.7,
+    'gamma': 0.99,
+    'hidden_dim': 64,
+    'learning_rate': 0.002,
+    'learning_starts': 0.09*100_000,
+    'target_update_interval': 100,
+    'tau': 0.97,
+    'gradient_steps': 2,
+    'train_freq': 2,
+}
+
+
 
 def main():
+    map_name = '4x4'
+    env_id = ModifiedFrozenLake(map_name=map_name)
+    env_id = TimeLimit(env_id, max_episode_steps=100)
+
     env_id = 'CartPole-v1'
     # env_id = 'Taxi-v3'
     # env_id = 'CliffWalking-v0'
@@ -26,15 +60,21 @@ def main():
     # env_id = 'FrozenLake-v1'
     # env_id = 'MountainCar-v0'
     # env_id = 'Drug-v0'
+    
+    if not isinstance(env_id, str):
+        env_str = map_name
+    else:
+        env_str = env_id
 
     wandb.init(project='clipping', entity='jacobhadamczyk', sync_tensorboard=True)
-    clip = True
-    wandb.log({'clip': clip})
-    agent = SoftQAgent(env_id, **sql_cpole, device='cuda', log_interval=500,
+    clip_method = 'hard'
+    pretrain = False
+    wandb.log({'clip_method': clip_method, 'env_id': env_str, 'pretrain': pretrain})
+    agent = SoftQAgent(env_id, **sql_cpole, device='cpu', log_interval=500,
                  tensorboard_log='pong', num_nets=2, render=False, aggregator='min',
-                 scheduler_str='none', clip_target=clip)
+                 scheduler_str='none', clip_method=clip_method, pretrain=pretrain)
     # Measure the time it takes to learn:
-    agent.learn(total_timesteps=20_000)
+    agent.learn(total_timesteps=30_000)
     wandb.finish()
 
 
