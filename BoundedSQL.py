e8cd94b61e9242fba2f1d1ac4a824f29a03c2e5d
JacobHA <jacobhadamczyk@gmail.com>
2024-02-23 08:52:42 -0500

    aggregate q funcs before logsumexp

--- BoundedSQL.py
+++ BoundedSQL.py
@@ -114,10 +114,11 @@ class SoftQAgent(BaseAgent):
                 self.logger.record(f"train/{name}_mean", vals.mean().item())
 
 
-            #TODO: put aggregation here
-            next_vs = 1/self.beta * (torch.logsumexp(
-                self.beta * target_next_softqs, dim=-1) - torch.log(torch.Tensor([self.nA])).to(self.device))
-            next_v = self.aggregator_fn(next_vs, dim=0)
+            # aggregate the target next softqs:
+            target_next_softq = self.aggregator_fn(target_next_softqs, dim=0)
+            next_v = 1/self.beta * (torch.logsumexp(
+                self.beta * target_next_softq, dim=-1) - torch.log(torch.Tensor([self.nA])).to(self.device))
+            # next_v = self.aggregator_fn(next_vs, dim=0)
 
             next_v = next_v.reshape(-1, 1)
 
